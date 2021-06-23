3a61fd0d9f74c08be3cf05c1798e9aa4b658e14d
Max Abdelmonam <mohammedalaa159@gmail.com>
2021-06-24 07:10:54 +0800

    Added Stock Merchant problem

---
+++ sock_merchant.js
@@ -0,0 +1,49 @@
+// Complete the sockMerchant function below.
+
+
+// Solution 1
+// Sort the array and count 
+function sortAndCount( n, arr ){
+    let sorted = arr.sort( (a,b) => a - b );
+    let pairs = 0;
+
+    for(let i = 0; i < n; i++ ) {
+        if ( sorted[i] === sorted[i+1] ){
+            pairs++;
+            i += 1;
+        }
+    }
+
+    return pairs;
+}
+
+// Solution 2
+// Stock the arr to new arr and count the even ones
+function stockAndCount( n, arr ){
+    let pairs = 0;
+    const colors = arr.reduce( (acc, val) => {
+        (!!acc[val]) ? acc[val] += 1 : acc[val] = 1;
+        return acc;
+    }, {} )
+
+    Object.keys(colors).forEach ( n => {
+        let _pair = parseInt( colors[n] / 2);
+        if ( _pair >= 1 ) pairs += _pair;
+    })
+
+    return pairs;
+}
+
+const n = 9;
+const arr = [10,20,20,10,10,30,50,10,20];
+
+console.clear();
+
+console.group('Sorted and counted');
+    console.log(`There are a total of ${sortAndCount( n, arr )} pairs`);
+console.groupEnd();
+
+
+console.group('stockAndCount');
+    console.log(`There are a total of ${stockAndCount( n, arr )} Pairs`)
+console.groupEnd();
