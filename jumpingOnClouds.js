48ef69eacc21ad2e8a118bb14a97461350918ada
Max Abdelmonam <mohammedalaa159@gmail.com>
2021-06-24 20:34:16 +0800

    added Jumping on the clouds problem

---
+++ jumpingOnClouds.js
@@ -0,0 +1,26 @@
+// Complete the countingVallys function below.
+
+function jumpingOnClouds(c){
+    let j = 0;
+
+    for (let i = c.length; --i > 0; ++j){
+        if (c[i - 2] == 0){
+            --i;
+        }
+    }
+    return j;
+}
+
+console.clear();
+
+console.group('Test case 1');
+    console.log(`you can jump on ${jumpingOnClouds([0,1,0,0,0,1,0])} cloud`);
+console.groupEnd();
+
+console.group('Test case 2');
+    console.log(`you can jump on ${jumpingOnClouds([0,0,1,0,0,1,0])} cloud`);
+console.groupEnd();
+
+console.group('Test case 3');
+    console.log(`you can jump on ${jumpingOnClouds([0,0,0,0,1,0])} cloud`);
+console.groupEnd();
